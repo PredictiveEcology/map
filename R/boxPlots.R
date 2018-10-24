cd4ca7b35b3542551ce1893b675b7f096b7c58e8
Eliot McIntire <eliotmcintire@gmail.com>
2018-10-24 12:50:36 -0700

    boxPlots

--- R/boxPlots.R
+++ R/boxPlots.R
@@ -1,3 +1,4 @@
+#' @export
 .doPlotBoxplot <- function(data, CCpnts = NULL, authStatus, fname = NULL, ...) {
   if (!is.null(fname)) png(fname, height = 600, width = 800, units = "px")
   boxplot(proportion~as.factor(ageClass), data, ...)
@@ -12,13 +13,21 @@
   if (!is.null(fname)) dev.off()
 }
 
-runBoxPlotsVegCover <- function(map) {
+#' @export
+runBoxPlotsVegCover <- function(map, dt, analysisGroups) {
   browser()
-  map
-  data[, .doPlotBoxplot(data = .SD,  authStatus = TRUE, col = "limegreen",
+
+  allRepPolys <- na.omit(map@metadata[[analysisGroups]])
+  names(allRepPolys) <- allRepPolys
+
+  # Here is hard coded 1 data.table -- should be in Lapply
+  data <- map@analysesData[[dt]][["LeadingVegTypeByAgeClass"]]$`Small Study Area`
+  par(mfrow = c(3,4))
+  out <- data[, .doPlotBoxplot(data = .SD, authStatus = TRUE, col = "limegreen",
                         horizontal = TRUE,
                         main = unique(paste(zone, vegCover, collapse = " ")), ## TODO: add total area to title (#59)
                         xlab = paste0("Proportion of of forest area (total ", sum(NPixels, na.rm= TRUE) * res(rasterToMatch(ml))[1]^2/1e4 , " ha)"),
                         ylab = "Age class",
                         ylim = c(0, 1)), .SDcols = c("ageClass", "proportion", "NPixels"), by = c("zone", "vegCover")]
+  out
 }
