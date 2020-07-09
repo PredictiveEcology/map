4f46646806e2dac58bbd128c7610fe77d2f4ba95
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-07-09 14:34:13 -0700

    makeTiles -- updates to Windows "finding" of gdal tools

--- R/makeTiles.R
+++ R/makeTiles.R
@@ -1,34 +1,60 @@
+#' Make tiles (pyramids) using gdal2tiles
+#'
 #' @importFrom raster projectRaster writeRaster
 #' @importFrom sp CRS
 #' @importFrom tiler tile
-makeTiles <- function(tilePath, obj) {
-  dirNotExist <- !dir.exists(tilePath)
+#' @param tilePath A director to write tiles
+#' @param obj A raster objects with or without file-backing
+#' @param overwrite Logical. If \code{FALSE}, and the director exists,
+#'   then it will not overwrite any files.
+#' @param ... Passed to reproducible::projectInputs e.g., \code{useGDAL}
+#' @export
+makeTiles <- function(tilePath, obj, overwrite = FALSE, ...) {
+  dirNotExist <- !dir.exists(tilePath) | isTRUE(overwrite)
 
   if (dirNotExist) { # assume that tilePath is unique for that obj, via .robustDigest
     if (reproducible:::isWindows()) {
       out <- reproducible:::findGDAL()
       if (isFALSE(out))
         stop("Need to have gdal installed; see ?tiler")
-      tiler::tiler_options(
-        python =
-          file.path(getOption("gdalUtils_gdalPath")[[1]]$path, "python.exe"))
+      pydir <- file.path(getOption("gdalUtils_gdalPath")[[1]]$path)
+      possPyBin <- file.path(pydir, "python.exe")
+      possPyBin3 <- file.path(pydir, "python3.exe")
+
+      pythonPth <- if (file.exists(possPyBin)) {
+        possPyBin
+      } else if (file.exists(possPyBin3)) {
+        possPyBin3
+      } else {
+        stop("Can't find python.exe or python3.exe")
+      }
+      tiler::tiler_options(python = pythonPth)
+
       out <- findOSGeo4W() # set path in tiler::tiler_options
       if (length(out) == 0)
         stop("Need to have OSGeo4W installed; see ?tiler")
     }
     obj[] <- obj[]
     message("  Creating tiles - reprojecting to epsg:4326 (leaflet projection)")
-    message("                   writing to disk")
-    objLflt <- try(projectRaster(obj, crs = CRS("+init=epsg:4326")), silent = TRUE)
-    tmpFile <- tempfile(fileext = ".tif")
-    objLflt <- try(writeRaster(objLflt, tmpFile), silent = TRUE)
+    objLflt <- try(projectInputs(obj, targetCRS = CRS("+init=epsg:4326"), ...), silent = TRUE)
+    # objLflt <- try(projectRaster(obj, crs = CRS("+init=epsg:4326")), silent = TRUE)
+    if (nchar(filename(objLflt)) == 0) {
+      tmpFile <- tempfile(fileext = ".tif")
+      message("                   writing to disk")
+      objLflt <- try(writeRaster(objLflt, tmpFile), silent = TRUE)
+    } else {
+      tmpFile <- filename(objLflt)
+    }
 
     toDo <- TRUE
     tryNum <- 1
     while (toDo) {
       print(tryNum)
-      out <- try(tiler::tile(tmpFile, tilePath, zoom = "1-10", crs = CRS("+init=epsg:4326"),
-                  format = "tms", viewer = FALSE, resume = TRUE), silent = TRUE)
+      isCorrectCRS <- compareCRS(CRS("+init=epsg:4326"), objLflt)
+      browser()
+      out <- try(tiler::tile(tmpFile, tilePath, zoom = "1-10",
+                             crs = CRS("+init=epsg:4326"),
+                             format = "tms", viewer = FALSE, resume = TRUE), silent = TRUE)
       toDo <- is(out, "try-error")
       files <- dir(tilePath, recursive = TRUE)
       if (length(files) < 5) {
